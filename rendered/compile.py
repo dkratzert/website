b4e7954b0450c3ebef4e06c7683b5db047a16614
Daniel Kratzert <dkratzert@gmx.de>
2021-09-22 22:45:19 +0200

    first working version

--- rendered/compile.py
+++
@@ -1,7 +0,0 @@
-from staticjinja import Site
-
-
-if __name__ == "__main__":
-    site = Site.make_site(searchpath='.', outpath='./rendered')
-    # enable automatic reloading
-    site.render(use_reloader=True)
