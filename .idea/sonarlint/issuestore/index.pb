4a8917e4d4f4e2031ba66607f3abe5b3a006a560
Daniel Kratzert <dkratzert@gmx.de>
2021-09-22 21:50:36 +0200

    no idea

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-B
-engine/__init__.py,6/c/6c1b3613f0ab1b6d964fd304fc44c42f517c14d6
--- dkratzert/compile.py
+++ dkratzert/compile.py
@@ -2,6 +2,6 @@ from staticjinja import Site
 
 
 if __name__ == "__main__":
-    site = Site.make_site(searchpath='..', outpath='../rendered')
+    site = Site.make_site(searchpath='.', outpath='./rendered')
     # enable automatic reloading
     site.render(use_reloader=True)
